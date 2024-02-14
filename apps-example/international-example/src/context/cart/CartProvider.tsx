28fe0c3ac807524c35e95fde647d6d5b7a6eae46
marulloc <marullo@naver.com>
2024-02-14 23:14:48 +0900

    refactor : Split CartContext into CartMutationContext and CartQueryContext

--- apps-example/international-example/src/context/cart/CartProvider.tsx
+++ apps-example/international-example/src/context/cart/CartProvider.tsx
@@ -1,10 +1,25 @@
 'use client';
 
 import { useCallback, useEffect, useState } from 'react';
-import { CartContext, CartContextType } from './context';
-import { addToCart, removeFromCart, updateCart } from '@/@marulloc-shopify-nextapi/v24.01/services/cart/service';
+import {
+  CartContext,
+  CartContextType,
+  CartMutationContext,
+  CartQueryContext,
+  TCartMutationContext,
+  TCartQueryContext,
+} from './context';
+import {
+  addToCart,
+  createCart,
+  getCart,
+  removeFromCart,
+  updateCart,
+  updateCartLocale,
+} from '@/@marulloc-shopify-nextapi/v24.01/services/cart/service';
 import { ShopifyLocaleContext } from '@/@marulloc-shopify-nextapi/v24.01/@shopify-types/shopify-common';
 import { useCartInitEffect, useCartLocaleEffect } from './hooks';
+import { ToolkitCart } from '@/@marulloc-shopify-nextapi/v24.01/services/@toolkit-types/toolkit-cart';
 
 type Props = {
   children: React.ReactNode;
@@ -67,3 +82,75 @@ const CartProvider = ({ children, locale }: Props) => {
 };
 
 export default CartProvider;
+
+type TProps = {
+  children: React.ReactNode;
+  locale?: ShopifyLocaleContext;
+  storageKey: string;
+};
+export const CartProvider2 = ({ children, locale, storageKey }: TProps) => {
+  const [cartQueryContext, setCartQueryContext] = useState<TCartQueryContext>({ cart: null, status: 'pending' });
+
+  const initCart = async () => {
+    const memoizedCart = JSON.parse(localStorage.getItem(storageKey) || 'null') as ToolkitCart | null;
+
+    let savedOrNewCart: ToolkitCart;
+    if (memoizedCart) savedOrNewCart = await getCart(memoizedCart.id, locale);
+    else savedOrNewCart = await createCart(locale);
+    setCartQueryContext({ status: 'loaded', cart: savedOrNewCart });
+  };
+
+  const memoCart = async (cart: TCartQueryContext['cart']) => {
+    if (!cart) return;
+    localStorage.setItem(storageKey, JSON.stringify(cart));
+  };
+
+  const updateLocale = async () => {
+    if (!cartQueryContext.cart || !locale) return;
+    if (locale.country?.toLowerCase() === cartQueryContext.cart?.buyerIdentity?.countryCode.toLowerCase()) return;
+
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, status: 'pending' }));
+    const cartWithNewLocale = await updateCartLocale(cartQueryContext.cart.id, locale);
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, cart: cartWithNewLocale, status: 'loaded' }));
+  };
+
+  const addItem: CartContextType['addItem'] = async ({ variantId, quantity }) => {
+    if (!cartQueryContext.cart) return;
+
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, status: 'pending' }));
+    const newCart = await addToCart(cartQueryContext.cart.id, [{ merchandiseId: variantId, quantity }], locale);
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, cart: newCart, status: 'loaded' }));
+  };
+
+  const updateItem: CartContextType['updateItem'] = async ({ lineId, quantity }) => {
+    if (!cartQueryContext.cart) return;
+
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, status: 'pending' }));
+    const newCart = await updateCart(cartQueryContext.cart.id, [{ id: lineId, quantity }], locale);
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, cart: newCart, status: 'loaded' }));
+  };
+
+  const deleteItem: CartContextType['deleteItem'] = async ({ lineId }) => {
+    if (!cartQueryContext.cart) return;
+
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, status: 'pending' }));
+    const newCart = await removeFromCart(cartQueryContext.cart.id, [lineId], locale);
+    setCartQueryContext(({ status, ...rest }) => ({ ...rest, cart: newCart, status: 'loaded' }));
+  };
+
+  useEffect(() => {
+    initCart();
+    updateLocale();
+
+    return () => {
+      memoCart(cartQueryContext.cart);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cartQueryContext.cart, locale]);
+
+  return (
+    <CartMutationContext.Provider value={{ addItem, updateItem, deleteItem }}>
+      <CartQueryContext.Provider value={cartQueryContext}>{children}</CartQueryContext.Provider>
+    </CartMutationContext.Provider>
+  );
+};
--- apps-example/international-example/src/context/cart/context.ts
+++ apps-example/international-example/src/context/cart/context.ts
@@ -36,3 +36,42 @@ export const useCartContext = () => {
 
   return context;
 };
+
+// Refactoring
+export type TCartMutationContext = {
+  addItem: ({ variantId, quantity }: { variantId: string; quantity: number }) => Promise<void>;
+  updateItem: ({ lineId, quantity }: { lineId: string; quantity: number }) => Promise<void>;
+  deleteItem: ({ lineId }: { lineId: string }) => Promise<void>;
+};
+
+export const CartMutationContext = createContext<TCartMutationContext>({
+  addItem: async () => {},
+  updateItem: async () => {},
+  deleteItem: async () => {},
+});
+
+export const useCartMutation = () => {
+  const context = useContext(CartMutationContext);
+
+  if (!context) {
+    throw new Error('useCartActions must be used within a CartActionsProvider');
+  }
+  return context;
+};
+
+export type TCartQueryContext =
+  | { status: 'pending'; cart: null | ToolkitCart }
+  | { status: 'loaded'; cart: ToolkitCart };
+
+export const CartQueryContext = createContext<TCartQueryContext>({
+  status: 'pending',
+  cart: null,
+});
+
+export const useCartQuery = () => {
+  const context = useContext(CartQueryContext);
+  if (!context) {
+    throw new Error('useCartData must be used within a CartDataProvider');
+  }
+  return context;
+};
