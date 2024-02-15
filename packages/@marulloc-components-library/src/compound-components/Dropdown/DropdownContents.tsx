aacb96db94f1f23195f2485a71a2a85b3849d67c
marulloc <marullo@naver.com>
2024-02-15 15:45:36 +0900

    chore : dropdown Compound component overflow position mounted... temp resolve : block <->hidden style

--- packages/@marulloc-components-library/src/compound-components/Dropdown/DropdownContents.tsx
+++ packages/@marulloc-components-library/src/compound-components/Dropdown/DropdownContents.tsx
@@ -11,40 +11,46 @@ type DropdownContentsProps = {
   matchTriggerWidth?: boolean;
 } & Omit<React.ComponentPropsWithoutRef<'div'>, 'children' | 'id'>;
 
+/**
+ * @bugToDo not-mounted sylte 없이 애니메이션 동작하게해야함, 현재는 hidden 처리 안하면 최초에 overflow 된 위치에 박스생성되어있음
+ * @param param0
+ * @returns
+ */
 const DropdownContents = ({ children, className, matchTriggerWidth = true, ...rest }: DropdownContentsProps) => {
   const { isOpen, dropdownId, triggerId, ...restContext } = useDropdownContext();
   const [isMounted, setIsMounted] = useState(false);
 
+  const updatePosition = () => {
+    const dropdown = document.getElementById(dropdownId);
+    const trigger = document.getElementById(triggerId);
+
+    if (!dropdown || !trigger) return;
+
+    const triggerRect = trigger.getBoundingClientRect();
+    const dropdownHeight = dropdown.offsetHeight;
+    const spaceBelow = window.innerHeight - triggerRect.bottom;
+
+    if (spaceBelow < dropdownHeight && triggerRect.top > dropdownHeight) {
+      // 현재 뷰포트보다 드롭다운의 높이가 클 경우에
+      dropdown.style.top = `${triggerRect.top - dropdownHeight + window.scrollY}px`;
+    } else {
+      dropdown.style.top = `${triggerRect.bottom + window.scrollY}px`;
+    }
+
+    dropdown.style.left = `${triggerRect.left + window.scrollX}px`;
+    if (matchTriggerWidth) dropdown.style.width = `${triggerRect.width}px`;
+  };
+  if (isOpen) {
+    updatePosition();
+    window.addEventListener('resize', updatePosition);
+  }
   useEffect(() => {
+    updatePosition();
     setIsMounted(true);
   }, []);
 
   useEffect(() => {
-    const updatePosition = () => {
-      const dropdown = document.getElementById(dropdownId);
-      const trigger = document.getElementById(triggerId);
-
-      if (!dropdown || !trigger) return;
-
-      const triggerRect = trigger.getBoundingClientRect();
-      const dropdownHeight = dropdown.offsetHeight;
-      const spaceBelow = window.innerHeight - triggerRect.bottom;
-
-      if (spaceBelow < dropdownHeight && triggerRect.top > dropdownHeight) {
-        // 현재 뷰포트보다 드롭다운의 높이가 클 경우에
-        dropdown.style.top = `${triggerRect.top - dropdownHeight + window.scrollY}px`;
-      } else {
-        dropdown.style.top = `${triggerRect.bottom + window.scrollY}px`;
-      }
-
-      dropdown.style.left = `${triggerRect.left + window.scrollX}px`;
-      if (matchTriggerWidth) dropdown.style.width = `${triggerRect.width}px`;
-    };
-    if (isOpen) {
-      updatePosition();
-      window.addEventListener('resize', updatePosition);
-    }
-
+    updatePosition();
     return () => window.removeEventListener('resize', updatePosition);
   }, [dropdownId, isOpen, matchTriggerWidth, triggerId]);
 
@@ -56,8 +62,8 @@ const DropdownContents = ({ children, className, matchTriggerWidth = true, ...re
         {...rest}
         id={dropdownId}
         className={classNames(
-          'dropdown-contents',
-          isOpen ? 'dropdown-contents-visible' : 'dropdown-contents-invisible',
+          isMounted ? 'dropdown-not-mounted' : 'dropdown-contents',
+          isMounted && isOpen ? 'dropdown-contents-visible' : 'dropdown-contents-invisible',
           className,
         )}
       >
