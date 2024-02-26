6675bce5c9a6b96aa8a8b6407d7780eca5b3507a
marulloc <marullo@naver.com>
2024-02-27 06:25:34 +0900

    feature : Pages SEO metadata

--- apps-example/international-example/src/app/[locale]/[page]/page.tsx
+++ apps-example/international-example/src/app/[locale]/[page]/page.tsx
@@ -2,37 +2,61 @@ import { getPage } from '@/@marulloc-shopify-nextapi/v24.01/services/page/servic
 import { localTheme } from '@/theme/local-theme';
 import { splitLocale } from '@/utils/locale';
 import { classNames } from '@marulloc/components-library/utils';
+import { Metadata, ServerRuntime } from 'next';
+import { notFound } from 'next/navigation';
 
-const StaticPage = async ({ params }: { params: { page: string; locale: string } }) => {
+export const runtime: ServerRuntime = 'edge';
+
+type TPageParams = {
+  params: { page: string; locale: string };
+};
+
+export const generateMetadata = async ({ params }: TPageParams): Promise<Metadata> => {
+  const { countryCode, languageCode } = splitLocale(params.locale);
+  const { page: pageHandle } = params;
+
+  const page = await getPage(pageHandle, { country: countryCode.toUpperCase(), language: languageCode.toUpperCase() });
+
+  if (!page) return notFound();
+  return {
+    title: page.seo.title || page.title,
+    description: page.seo.description || page.bodySummary,
+    openGraph: {
+      type: 'article',
+      modifiedTime: page.updatedAt,
+      publishedTime: page.createdAt,
+    },
+  };
+};
+
+const StaticPage = async ({ params }: TPageParams) => {
   const { countryCode, languageCode } = splitLocale(params.locale);
   const { page: pageHandle } = params;
 
   const page = await getPage(pageHandle, { country: countryCode.toUpperCase(), language: languageCode.toUpperCase() });
 
   return (
-    <main>
-      <section>
-        <div className="bg-indigo-100 text-indigo-600 rounded-lg p-6 mb-1">
-          <h1 className={classNames('text-2xl sm:text-3xl md:text-4xl font-bold pb-3 mb-3 border-b')}>{page.title}</h1>
-          <p className={classNames(localTheme.text.size.small, 'text-left')}> {page.bodySummary}</p>
-        </div>
+    <section>
+      <div className="bg-indigo-100 text-indigo-600 rounded-lg p-6 mb-1">
+        <h1 className={classNames('text-2xl sm:text-3xl md:text-4xl font-bold pb-3 mb-3 border-b')}>{page.title}</h1>
+        <p className={classNames(localTheme.text.size.small, 'text-left')}> {page.bodySummary}</p>
+      </div>
 
-        <div className={classNames(localTheme.text.size.small, 'text-right mb-3 text-xs text-indigo-400')}>
-          <p className=" text-right mt-1 text-xs  ">
-            {`Last update : ${new Intl.DateTimeFormat(languageCode, {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            }).format(new Date(page.updatedAt))}.`}
-          </p>
-        </div>
+      <div className={classNames(localTheme.text.size.small, 'text-right mb-3 text-xs text-indigo-400')}>
+        <p className=" text-right mt-1 text-xs  ">
+          {`Last update : ${new Intl.DateTimeFormat(languageCode, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }).format(new Date(page.updatedAt))}.`}
+        </p>
+      </div>
 
-        <div
-          dangerouslySetInnerHTML={{ __html: page.body }}
-          className="prose bg-gray-100 text-gray-600 rounded-lg p-6 "
-        />
-      </section>
-    </main>
+      <div
+        dangerouslySetInnerHTML={{ __html: page.body }}
+        className="prose bg-gray-100 text-gray-600 rounded-lg p-6 "
+      />
+    </section>
   );
 };
 
