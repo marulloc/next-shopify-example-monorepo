68da1d29d4cc2a2c0c32f1ebd8f776b327ec59da
marulloc <marullo@naver.com>
2024-02-28 17:06:31 +0900

    feature : static page(shopify page) loading skeleton

---
+++ apps-example/international-example/src/app/[locale]/[page]/loading.tsx
@@ -0,0 +1,87 @@
+import Card from '@/components/@common/semantic/Card';
+import Skeleton from '@/components/loading/Skeleton';
+import { localTheme } from '@/theme/local-theme';
+import { classNames } from '@marulloc/components-library/utils';
+
+const StaticPageLoading = () => {
+  return (
+    <main className={classNames('flex-1 flex flex-col md:flex-row  ', ' border-b', localTheme.border.base.main)}>
+      <div className={classNames('flex-1', localTheme.spacing.padding.xy.medium, 'max-w-4xl mx-auto')}>
+        <section className=" p-0 ">
+          <Card
+            as="div"
+            level={2}
+            className={classNames('border-0  shadow-md bg-indigo-200 text-indigo-800 rounded-lg p-6  ')}
+          >
+            <div className={classNames('h-6 w-1/3 pb-3 mb-3')}>
+              <Skeleton />
+            </div>
+            <div className="space-y-1">
+              <div className={classNames('h-2 w-2/3')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-2 w-2/3')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-2 w-1/3')}>
+                <Skeleton />
+              </div>
+            </div>
+          </Card>
+
+          <div className="prose mt-6 p-6">
+            <div className="space-y-1">
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-1/2')}>
+                <Skeleton />
+              </div>
+            </div>
+
+            <div className="space-y-1 mt-4">
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-1/2')}>
+                <Skeleton />
+              </div>
+            </div>
+
+            <div className="space-y-1 mt-4">
+              <div className={classNames('h-4 w-2/3')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-full')}>
+                <Skeleton />
+              </div>
+              <div className={classNames('h-4 w-1/4')}>
+                <Skeleton />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default StaticPageLoading;
--- apps-example/international-example/src/app/[locale]/[page]/page.tsx
+++ apps-example/international-example/src/app/[locale]/[page]/page.tsx
@@ -3,6 +3,7 @@ import Box from '@/components/@common/semantic/Box';
 import Card from '@/components/@common/semantic/Card';
 import { localTheme } from '@/theme/local-theme';
 import { splitLocale } from '@/utils/locale';
+import { delay } from '@/utils/throttle';
 import { classNames } from '@marulloc/components-library/utils';
 import { Metadata, ServerRuntime } from 'next';
 import { notFound } from 'next/navigation';
@@ -38,27 +39,29 @@ const StaticPage = async ({ params }: TPageParams) => {
   const [page] = await Promise.all([getPage(pageHandle, { country, language })]);
 
   return (
-    <div>
-      <p className=" text-right mb-2 text-xs  ">
-        {`Last update : ${new Intl.DateTimeFormat(language, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        }).format(new Date(page.updatedAt))}`}
-      </p>
-      <Card as="section" level={0} className="border-0 p-6 ">
-        <Card
-          as="header"
-          level={2}
-          className="border-0 -mx-6 -mt-6 shadow-md bg-indigo-200 text-indigo-800 rounded-lg p-6  "
-        >
-          <h1 className={classNames('text-xl sm:text-2xl md:text-3xl font-bold pb-3 mb-3')}>{page.title}</h1>
-          <p className={classNames('text-xs  w-full md:w-2/3', 'text-left')}> {page.bodySummary}</p>
-        </Card>
+    <main className={classNames('flex-1 flex flex-col md:flex-row  ', ' border-b', localTheme.border.base.main)}>
+      <div className={classNames('flex-1', localTheme.spacing.padding.xy.medium, 'max-w-4xl mx-auto')}>
+        <p className=" text-right mb-2 text-xs  ">
+          {`Last update : ${new Intl.DateTimeFormat(language, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }).format(new Date(page.updatedAt))}`}
+        </p>
+        <section className=" p-0 ">
+          <Card
+            as="header"
+            level={2}
+            className={classNames('border-0  shadow-md bg-indigo-200 text-indigo-800 rounded-lg p-6  ')}
+          >
+            <h1 className={classNames('text-xl sm:text-2xl md:text-3xl font-bold pb-3 mb-3')}>{page.title}</h1>
+            <p className={classNames('text-xs  w-full md:w-2/3', 'text-left')}> {page.bodySummary}</p>
+          </Card>
 
-        <div className="prose mt-6" dangerouslySetInnerHTML={{ __html: page.body }} />
-      </Card>
-    </div>
+          <div className="prose mt-6 p-6" dangerouslySetInnerHTML={{ __html: page.body }} />
+        </section>
+      </div>
+    </main>
   );
 };
 
