316685315bd133bd4276032a8c3f657dab51d96d
marulloc <marullo@naver.com>
2024-03-03 19:34:41 +0900

    chore :delete legacy project

--- apps-example/basic-example/src/services/common/parser.ts
+++
@@ -1,8 +0,0 @@
-export const parseMenuRoute = (menuItems?: Array<ShopifyMenuItem>) => {
-  return (
-    menuItems?.map((item) => ({
-      title: item.title,
-      url: item.url.replace(`https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}`, ''),
-    })) || []
-  );
-};
--- apps-example/basic-example/src/shopify-gql/@types/shopify-order.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @docs https://shopify.dev/docs/api/storefront/2024-01/objects/Order
- */
-type ShopifyOrder = {};
