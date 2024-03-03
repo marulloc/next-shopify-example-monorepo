34a0bc677810aebc2b255592474d0e93153b3f7b
marulloc <marullo@naver.com>
2024-03-03 17:47:35 +0900

    refactor: Remove direct dependency on env-variables

--- packages/@marulloc-shopify-next-api/v24.01/storeFetch.ts
+++ packages/@marulloc-shopify-next-api/v24.01/storeFetch.ts
@@ -1,21 +1,4 @@
-const storeDomain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || '';
-const apiVersion = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION || '';
-const endPoint = `https://${storeDomain}/api/${apiVersion}/graphql.json`;
-const accessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN || '';
-
-function checkEnvVariables() {
-  const requiredEnvVars = [
-    'NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN',
-    'NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION',
-    'NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN',
-  ];
-
-  const missingEnvVars = requiredEnvVars.filter((envVar) => !process.env[envVar]);
-
-  if (missingEnvVars.length > 0) {
-    throw new Error(`Missing environment variables: ${missingEnvVars.join(', ')}`);
-  }
-}
+import { getShopfiyNextConfig } from './storeConfig';
 
 export type StoreFetchParams<T> = {
   headers?: HeadersInit;
@@ -26,7 +9,8 @@ export type StoreFetchParams<T> = {
 };
 
 export const storeFetch = async <T>(params: StoreFetchParams<T>): Promise<{ status: number; body: T }> => {
-  // checkEnvVariables();
+  const { endPoint, accessToken } = getShopfiyNextConfig();
+
   const method = 'POST';
   const headers = {
     'X-Shopify-Storefront-Access-Token': accessToken,
